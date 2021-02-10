2a6c65f8038e1919831c0761fe2234c00f5d90c4
asnakesonic123 <76957392+asnakesonic123@users.noreply.github.com>
2021-02-10 10:33:04 -0500

    Update Core Interface.js

--- Interface/Core Interface.js
+++ Interface/Core Interface.js
@@ -1,1 +1,10 @@
 var atbtes = {null: 'null', core: 'javascript', interface: 'folder'}
+var MAX_ATTRIBUTES = "125"
+var MIN_ATTRIBUTES = "5"
+
+function Get_Inter(Inter_Level, inter_low) {
+	if (Inter_Level < inter_low) {
+		Inter_Level = 0
+		var inter_low = null
+		alert("Cannot Display More Than Actual")
+	}
